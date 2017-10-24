5211a3bb4f3a9beb04b1d473ade8c3c3244f750b
Hippolyte <fayolt@live.fr>
2017-10-24 22:19:45 +0300

    Part 1 fully working

--- web/controllers/customer_service_controller.ex
+++ web/controllers/customer_service_controller.ex
@@ -11,34 +11,29 @@ defmodule Takso.CustomerServiceController do
     render conn, "lost_and_found.html", changeset: changeset, trips: trips 
   end
   def lost_and_found(conn, %{"cs_request" => %{"date_of_trip" => date, "pickup_address" => pickup_address}}) do
-    changeset = CSRequest.changeset(%CSRequest{})
-    query = filter_by_date(date) 
-            |> filter_by_address(pickup_address)
+    changeset = CSRequest.changeset(%CSRequest{}, %{date_of_trip: date, pickup_address: pickup_address})
+    query = filtering(date, pickup_address)
     query = from q in query, select: q
     trips = Repo.all(query)
     render conn, "lost_and_found.html", changeset: changeset, trips: trips
   end 
-  defp filter_by_date(date) do
-    from t in Trip, where: t.date == ^date
-  end
-  defp filter_by_address(query, address) when address == "" do
-    query
-  end
-  defp filter_by_address(query, address) do
-    from t in query, where: t.pickup_address == ^address
-  end
-  defp filtering(date \\ "", address \\ "")
+  # defp filter_by_date(date) do
+  #   from t in Trip, where: t.date == ^date
+  # end
+  # defp filter_by_address(query, address) when address == "" do
+  #   query
+  # end
+  # defp filter_by_address(query, address) do
+  #   from t in query, where: t.pickup_address == ^address
+  # end
+  # defp filtering(date \\ "", address \\ "")
   defp filtering(date, "") do
-    IO.puts("one ...")
     from t in Trip, where: t.date == ^date
   end
   defp filtering("", address) do 
-    IO.puts("two ...")
     from t in Trip, where: t.pickup_address == ^address
-    
   end
   defp filtering(date, address) do
-    IO.puts("three ...")
     from t in Trip, where: t.date == ^date and t.pickup_address == ^address
   end
 end
